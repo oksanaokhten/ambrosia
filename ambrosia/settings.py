9939df325f798c1a7461056831a12261c6ad824c
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-11 11:22:57 +0000

    Added notification Css, shopping bag preview, and additional messages

--- ambrosia/settings.py
+++ ambrosia/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-FREE_DELIVERY_THRESHOLD = 35
+FREE_DELIVERY_THRESHOLD = 80
 STANDARD_DELIVERY_PERCENTAGE = 15
 
 # Default primary key field type
