2138c42a00774b81d338cb1be6f0f17d5bff4816
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-17 20:13:12 +0000

    Added field volume_weight to product class and to product details page

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     weight = models.BooleanField(default=False, null=True, blank=True)
+    volum_weight = models.CharField(max_length=10, null=True, blank=True)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
