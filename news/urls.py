11395779246e918fa8d34bfd8b94c03161d33549
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-23 17:13:56 +0000

    Added store news detail page, views and urls

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_news, name='news')
+    path('', views.all_news, name='news'),
+    path('<int:news_id>/', views.news_detail, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import News
 
 
@@ -12,3 +12,15 @@ def all_news(request):
     }
 
     return render(request, 'news/store_news.html', context)
+
+
+def news_detail(request, news_id):
+    """ A view to show individual store news details """
+
+    store_news = get_object_or_404(News, pk=news_id)
+
+    context = {
+        'store_news': store_news,
+    }
+
+    return render(request, 'news/store_news_detail.html', context)
