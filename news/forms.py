352617622e0bf59ea86777e81b66df45ec02f005
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-23 18:16:22 +0000

    News Admin - News Form

---
+++ news/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import News
+
+
+class NewsForm(forms.ModelForm):
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-dark rounded-0'    
