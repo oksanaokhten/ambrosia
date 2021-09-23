83f60a55f6d2c8f511a3144d00536b633f2b7aeb
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-23 12:11:25 +0000

    Added news views and templates

--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+
+def all_news(request):
+    """ A view to show all store news """
+
+    news = News.objects.all()
+    
+    context = {
+        'news': news,
+    }
+
+    return render(request, 'news/store_news.html', context)
