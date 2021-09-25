029714ad3bd1f1ee0dcd1c48bf98ba982f6ccfd2
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-25 16:35:06 +0000

    Back to start deployment

--- ambrosia/settings.py
+++ ambrosia/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-dh*swm&s2$=srhod^3cj^q^(^((f(+d0hrtr$-p09bhc4e5ii(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ambrosia-project.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,17 +124,12 @@ WSGI_APPLICATION = 'ambrosia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
