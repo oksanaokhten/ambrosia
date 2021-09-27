71f06ec7a15fe532785bbf9e35a2bd411c721e10
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-27 16:17:11 +0000

    Added secret key

--- ambrosia/settings.py
+++ ambrosia/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dh*swm&s2$=srhod^3cj^q^(^((f(+d0hrtr$-p09bhc4e5ii('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
