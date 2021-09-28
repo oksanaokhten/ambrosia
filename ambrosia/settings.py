1354aeff6b1f18b590a0cd9fc8afa7fccc8664b3
oksanaokhten <oksanaokhten@yahoo.com>
2021-09-28 09:22:24 +0000

    Removed secret key, set debug and fixed allowed host

--- ambrosia/settings.py
+++ ambrosia/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ao%_rm+(j#0#d%)=)d)8$w^h=wfo6ss8^0c7hr3z7rxp^09!8a'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ambrosia-main', 'localhost']
+ALLOWED_HOSTS = ['ambrosia-main.herokuapp.com', 'localhost']
 
 
 # Application definition
